89c08d0e124cb4695b3caf4a8d7e2be02847c4e1
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-01-10 21:02:32 +0200

    create scroll for the team members

--- script/main.js
+++ script/main.js
@@ -1,5 +1,17 @@
 "use strict";
+// Handle Icon
 const icon = document.querySelector('.icon'), links = document.querySelector('.menu');
 icon.addEventListener('click', () => {
     links.classList.toggle('display');
+});
+// Handle Arrows to slide the team members
+document.querySelector('#right').addEventListener('click', (e) => {
+    document.querySelector('.members').scrollLeft += 370;
+    document.querySelector('.members').style.scrollBehavior =
+        'smooth';
+});
+document.querySelector('#left').addEventListener('click', (e) => {
+    document.querySelector('.members').scrollLeft -= 370;
+    document.querySelector('.members').style.scrollBehavior =
+        'smooth';
 });
--- script/main.ts
+++ script/main.ts
@@ -1,6 +1,26 @@
+// Handle Icon
 const icon = document.querySelector('.icon') as HTMLElement,
   links = document.querySelector('.menu') as HTMLElement;
 
 icon.addEventListener('click', () => {
   links.classList.toggle('display');
 });
+
+// Handle Arrows to slide the team members
+(document.querySelector('#right') as HTMLElement).addEventListener(
+  'click',
+  (e) => {
+    (document.querySelector('.members') as HTMLElement).scrollLeft += 370;
+    (document.querySelector('.members') as HTMLElement).style.scrollBehavior =
+      'smooth';
+  }
+);
+
+(document.querySelector('#left') as HTMLElement).addEventListener(
+  'click',
+  (e) => {
+    (document.querySelector('.members') as HTMLElement).scrollLeft -= 370;
+    (document.querySelector('.members') as HTMLElement).style.scrollBehavior =
+      'smooth';
+  }
+);
