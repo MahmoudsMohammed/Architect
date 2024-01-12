a8f65e0adc785aedc9ad5cdc5358c023d5d989b3
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-01-12 17:27:13 +0200

    display spinner when page load

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 "use strict";
-const icon = document.querySelector('.icon'), links = document.querySelector('.menu'), right = document.querySelector('#right'), left = document.querySelector('#left'), members = document.querySelector('.members');
+const icon = document.querySelector('.icon'), links = document.querySelector('.menu'), right = document.querySelector('#right'), left = document.querySelector('#left'), members = document.querySelector('.members'), main = document.querySelector('.main'), load = document.querySelector('.loading');
 // Handle Icon
 icon.addEventListener('click', () => {
     links.classList.toggle('display');
@@ -12,4 +12,12 @@ right.addEventListener('click', (e) => {
 left.addEventListener('click', (e) => {
     members.scrollLeft -= 320;
     members.style.scrollBehavior = 'smooth';
+});
+// Handle the spinner
+document.addEventListener('DOMContentLoaded', () => {
+    main.style.display = 'none';
+    setTimeout(() => {
+        load.style.display = 'none';
+        main.style.display = 'block';
+    }, 4000);
 });
--- script/main.ts
+++ script/main.ts
@@ -2,7 +2,9 @@ const icon = document.querySelector('.icon') as HTMLElement,
   links = document.querySelector('.menu') as HTMLElement,
   right = document.querySelector('#right') as HTMLElement,
   left = document.querySelector('#left') as HTMLElement,
-  members = document.querySelector('.members') as HTMLElement;
+  members = document.querySelector('.members') as HTMLElement,
+  main = document.querySelector('.main') as HTMLElement,
+  load = document.querySelector('.loading') as HTMLElement;
 
 // Handle Icon
 icon.addEventListener('click', () => {
@@ -18,4 +20,13 @@ right.addEventListener('click', (e) => {
 left.addEventListener('click', (e) => {
   members.scrollLeft -= 320;
   members.style.scrollBehavior = 'smooth';
+});
+
+// Handle the spinner
+document.addEventListener('DOMContentLoaded', () => {
+  main.style.display = 'none';
+  setTimeout(() => {
+    load.style.display = 'none';
+    main.style.display = 'block';
+  }, 4000);
 });
