11bf1df8ccb4e74bd5d69f88b4b46afb60a6d3ee
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-01-12 14:18:37 +0200

    add to top button

--- script/main.js
+++ script/main.js
@@ -1,17 +1,22 @@
 "use strict";
+
+var icon = document.querySelector('.icon'),
+  links = document.querySelector('.menu'),
+  right = document.querySelector('#right'),
+  left = document.querySelector('#left'),
+  members = document.querySelector('.members');
+
 // Handle Icon
-const icon = document.querySelector('.icon'), links = document.querySelector('.menu');
-icon.addEventListener('click', () => {
-    links.classList.toggle('display');
+icon.addEventListener('click', function () {
+  links.classList.toggle('display');
 });
+
 // Handle Arrows to slide the team members
-document.querySelector('#right').addEventListener('click', (e) => {
-    document.querySelector('.members').scrollLeft += 370;
-    document.querySelector('.members').style.scrollBehavior =
-        'smooth';
+right.addEventListener('click', function (e) {
+  members.scrollLeft += 370;
+  members.style.scrollBehavior = 'smooth';
 });
-document.querySelector('#left').addEventListener('click', (e) => {
-    document.querySelector('.members').scrollLeft -= 370;
-    document.querySelector('.members').style.scrollBehavior =
-        'smooth';
+left.addEventListener('click', function (e) {
+  members.scrollLeft -= 370;
+  members.style.scrollBehavior = 'smooth';
 });
--- script/main.ts
+++ script/main.ts
@@ -1,26 +1,21 @@
-// Handle Icon
 const icon = document.querySelector('.icon') as HTMLElement,
-  links = document.querySelector('.menu') as HTMLElement;
+  links = document.querySelector('.menu') as HTMLElement,
+  right = document.querySelector('#right') as HTMLElement,
+  left = document.querySelector('#left') as HTMLElement,
+  members = document.querySelector('.members') as HTMLElement;
 
+// Handle Icon
 icon.addEventListener('click', () => {
   links.classList.toggle('display');
 });
 
 // Handle Arrows to slide the team members
-(document.querySelector('#right') as HTMLElement).addEventListener(
-  'click',
-  (e) => {
-    (document.querySelector('.members') as HTMLElement).scrollLeft += 370;
-    (document.querySelector('.members') as HTMLElement).style.scrollBehavior =
-      'smooth';
-  }
-);
+right.addEventListener('click', (e) => {
+  members.scrollLeft += 370;
+  members.style.scrollBehavior = 'smooth';
+});
 
-(document.querySelector('#left') as HTMLElement).addEventListener(
-  'click',
-  (e) => {
-    (document.querySelector('.members') as HTMLElement).scrollLeft -= 370;
-    (document.querySelector('.members') as HTMLElement).style.scrollBehavior =
-      'smooth';
-  }
-);
+left.addEventListener('click', (e) => {
+  members.scrollLeft -= 370;
+  members.style.scrollBehavior = 'smooth';
+});
